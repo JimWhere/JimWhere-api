92ffc8f56ecfc2e21a5b40aba0bb0f3041f3f8a3
Kim-sangjae <kimsangjae94114@gmail.com>
2025-12-04 15:40:30 +0900

    first commit

---
+++ jimwhere-api/src/main/java/com/jimwhere/jimwhereapi/JimwhereApiApplication.java
@@ -0,0 +1,13 @@
+package com.jimwhere.jimwhereapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimwhereApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimwhereApiApplication.class, args);
+    }
+
+}
---
+++ jimwhere-api/src/test/java/com/jimwhere/jimwhereapi/JimwhereApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimwhere.jimwhereapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimwhereApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
