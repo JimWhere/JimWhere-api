d00973009866e90ca83f5a735433d0ea9df4ce9e
Sangjae Kim <kimsangjae94114@gmail.com>
2025-12-05 00:09:10 +0900

    feat(user) : CRUD 중간커밋 userCREATE 구현

---
+++ jimwhere-api/src/main/java/com/jimwhere/api/user/dto/reqeust/UserCreateRequest.java
@@ -0,0 +1,13 @@
+package com.jimwhere.api.user.dto.reqeust;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UserCreateRequest {
+    private String userId;
+    private String password;
+    private String userPhoneNumber;
+    private long userBusinessNumber;
+}
