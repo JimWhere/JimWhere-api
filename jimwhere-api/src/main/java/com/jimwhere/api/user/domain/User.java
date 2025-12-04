72cd11467324c8c0efd628b31bbdff0018343947
Sangjae Kim <kimsangjae94114@gmail.com>
2025-12-04 23:32:27 +0900

    feat(user) : 엔티티 생성

--- jimwhere-api/src/main/java/com/jimwhere/api/user/domain/User.java
+++ jimwhere-api/src/main/java/com/jimwhere/api/user/domain/User.java
@@ -1,17 +1,19 @@
 package com.jimwhere.api.user.domain;
 
+import com.jimwhere.api.global.model.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
 @Entity
 @Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,9 +25,33 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
+    private String userPhoneNumber;
+
+    @Column(nullable = false)
+    private Long userBusinessNumber;
+
     // DB에는 VARCHAR("USER", "ADMIN") 로 저장됨
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserRole role;
+
+    @Column(nullable = false)
+    private UserStatus status;
+
+
+    // 기본값 세팅을 위한 유저생성 메서드
+    public static User createUser(
+            String userId, String password, String userPhoneNumber, Long userBusinessNumber){
+
+        return User.builder()
+                .userId(userId)
+                .password(password)
+                .userPhoneNumber(userPhoneNumber)
+                .userBusinessNumber(userBusinessNumber)
+                .role(UserRole.USER)
+                .status(UserStatus.Y)
+                .build();
+    }
 
 }
