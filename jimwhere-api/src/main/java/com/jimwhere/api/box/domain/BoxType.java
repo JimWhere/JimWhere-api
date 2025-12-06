c12ac0c39ec2d36dcfa70a806d10d94034411005
silentanderson <anderson91@naver.com>
2025-12-06 22:36:39 +0900

    feat:룸 & 박스 엔티티 재생성

---
+++ jimwhere-api/src/main/java/com/jimwhere/api/box/domain/BoxType.java
@@ -0,0 +1,38 @@
+package com.jimwhere.api.box.domain;
+
+import com.jimwhere.api.global.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "box_type")
+@Getter
+@Setter
+@NoArgsConstructor
+public class BoxType extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "box_type_code")
+    private Long boxTypeCode;
+
+    @Column(name = "box_type_name")
+    private String boxTypeName;
+
+    @Column(name = "box_type_width")
+    private Long boxTypeWidth;
+
+    @Column(name = "box_type_length")
+    private Long boxTypeLength;
+
+    @Column(name = "box_type_height")
+    private Long boxTypeHeight;
+
+}
