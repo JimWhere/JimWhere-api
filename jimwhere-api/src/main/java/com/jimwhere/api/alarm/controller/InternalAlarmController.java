30a619d9bd13e0e215df2a698a9d8b334b8845bf
MADOBEDESK\madob <madobefamily@gmail.com>
2025-12-08 20:39:46 +0900

    feature alarm service3
    중간 저장

---
+++ jimwhere-api/src/main/java/com/jimwhere/api/alarm/controller/InternalAlarmController.java
@@ -0,0 +1,21 @@
+package com.jimwhere.api.alarm.controller;
+
+import com.jimwhere.api.alarm.domain.Alarm;
+import com.jimwhere.api.alarm.dto.AlarmCreateRequest;
+import com.jimwhere.api.alarm.service.AlarmService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/internal/alarms")
+public class InternalAlarmController {
+
+    private final AlarmService alarmService;
+
+    /* 알람 생성 (내부 서비스 전용) */
+    @PostMapping
+    public Alarm create(@RequestBody AlarmCreateRequest request) {
+        return alarmService.createAlarm(request);
+    }
+}
