9723f0a7440aa1bb8c54da78b3840c8e4866ff9d
silentanderson <anderson91@naver.com>
2025-12-08 21:41:38 +0900

    fix: RoomRepository 카피 폴더 삭제

--- jimwhere-api/src/main/java/com/jimwhere/api/room/repository copy/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jimwhere.api.room.repository;
-
-import com.jimwhere.api.room.domain.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
